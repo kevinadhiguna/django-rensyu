2bba11a85cbcd6943f3a09408a993363605b1f95
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-24 12:50:06 +0700

    refactor: register todo list page url

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home") # Calls 'home()' in views.py
+    path("", views.home, name="home"), # Calls 'home()' in views.py
+    path("todos/", views.todos, name="Todos")
 ]
 
