1bd8650a77236522796945407d0cf52264247cc0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-24 11:36:22 +0700

    refactor: render homepage

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-  return HttpResponse("Hello, World!")
+  # return HttpResponse("Hello, World")
+  return render(request, "home.html") # Renders 'home.html' in the templates directory
