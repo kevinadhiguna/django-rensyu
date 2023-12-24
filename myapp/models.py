81d4ac41c5825586a5fbd89344b8e2f742b07fdd
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-24 11:43:17 +0700

    chore(note): add a valuable note in order to makemigrations everytime a database model change takes place

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class TodoItem(models.Model):
   title = models.CharField(max_length=200)
   completed = models.BooleanField(default=False)
+
+
+# ################################################ #
+# Run: python3 manage.py makemigrations            #
+# Everytime you make a change to a database model! #
+# ################################################ #
